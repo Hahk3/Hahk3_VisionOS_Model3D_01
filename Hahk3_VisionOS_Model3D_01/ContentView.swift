4e43101622d0076791891cf598d252d4054800f3
Hahk3 <timelesstm@naver.com>
2023-08-08 06:58:51 +0900

    Initial Commit

---
+++ Hahk3_VisionOS_Model3D_01/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Hahk3_VisionOS_Model3D_01
+//
+//  Created by Hakjin Kim on 8/8/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hahk3_VisionOS_Model3D_01/Hahk3_VisionOS_Model3D_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Hahk3_VisionOS_Model3D_01App.swift
+//  Hahk3_VisionOS_Model3D_01
+//
+//  Created by Hakjin Kim on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Hahk3_VisionOS_Model3D_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
